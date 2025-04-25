a2adc5b8fb88ce9da3e5926eea36811813a45553
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-04-25 23:19:29 +0530

    Added

--- yolov8_test.py
+++ yolov8_test.py
@@ -1,3 +1,9 @@
 "https://subwaysurfersgame.io/"
 
 "NEED TO ASSIGN SOME ONE THIS ANNOTATION PART"
+
+URLS = [
+    "https://app.roboflow.com/subwaysurfers-ifgoh/subway-surfer-annotation/2",
+    "https://app.roboflow.com/collab-aurig/game_theory/2",
+    "https://app.roboflow.com/anything-lvnnu/object-detection-in-subway-sufer/1"
+    ]
