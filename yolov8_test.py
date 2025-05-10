a1e076844c451805932a049dfda2b7f59a5e5c4c
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-05-10 08:48:16 +0530

    Added Subway Surfers newer better links

--- yolov8_test.py
+++ yolov8_test.py
@@ -1,9 +1,5 @@
-"https://subwaysurfersgame.io/"
-
-"NEED TO ASSIGN SOME ONE THIS ANNOTATION PART"
+"https://subwaysurfersgame.io/subway-surf-zurich"
 
 URLS = [
-    "https://app.roboflow.com/subwaysurfers-ifgoh/subway-surfer-annotation/2",
-    "https://app.roboflow.com/collab-aurig/game_theory/2",
-    "https://app.roboflow.com/anything-lvnnu/object-detection-in-subway-sufer/1"
+    "https://universe.roboflow.com/daddy-ac8cj/subway_surfers/dataset/2"
     ]
