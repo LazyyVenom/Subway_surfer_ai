517d657f6de3f5058b4daa1616287653d7c2d9f0
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-05-13 11:00:05 +0530

    Added Condition for aftermath little bit later

--- Obj_test_custom.py
+++ Obj_test_custom.py
@@ -1,16 +1,23 @@
 import cv2
 from ultralytics import YOLO
+import mss
+import numpy as np
 
 model = YOLO('models/YoloV8/V4.pt')
 
 names = model.names
 
+monitor = {"top": 150, "left": 20, "width": 950, "height": 750}
+
+sct = mss.mss()
+
 cap = cv2.VideoCapture("videos\\final1.mp4")
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    screen_shot = sct.grab(monitor)
+
+    frame = np.array(screen_shot)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
 
     results = model(frame, verbose=False)
     boxes = results[0].boxes
@@ -21,8 +28,12 @@ while True:
 
         x1, y1, x2, y2 = map(int, box)
 
-        if y2 > 500:
-            print(f"Class: {class_name}, Lower Y (y2): {y2}")
+        if y2 > 400 and "Train" in class_name:
+            print(f"Train, Lower Y (y2): {y2}")
+
+
+        if y2 > 380 and "Obs" in class_name:
+            print(f"{class_name.replace("Obs_","")}, Lower Y (y2): {y2}")
 
         (text_w, text_h), baseline = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
         cv2.rectangle(frame, (x1, y1 - text_h - 6), (x1 + text_w + 4, y1), (0, 0, 0), -1)
