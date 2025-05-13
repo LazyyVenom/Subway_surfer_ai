9a086f8e05f1bf015c46ad2d3fd32407ce68c6e0
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-05-13 23:07:40 +0530

    Added Code for better running

--- Custom_Runner.py
+++ Custom_Runner.py
@@ -3,110 +3,98 @@ from ultralytics import YOLO
 import mss
 import numpy as np
 import math
+import time
 import pyautogui
-import random
 
-model = YOLO('models/YoloV8/V4.pt')
+model = YOLO('models/YoloV8/V5.pt')
 names = model.names
-monitor = {"top": 150, "left": 20, "width": 950, "height": 750}
+monitor = {"top": 200, "left": 30, "width": 850, "height": 620}
+middle_lane_room = 20
 
 sct = mss.mss()
 cap = cv2.VideoCapture("videos\\final1.mp4")
+current_lane = 1
+character_last_seen = time.time()
 
 while True:
-    current_lane = 1
     screen_shot = sct.grab(monitor)
-
     frame = np.array(screen_shot)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-
     results = model(frame, verbose=False)
     boxes = results[0].boxes
+    total_obstacles = []
+
+    char_details = {
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": 0,
+        "character_here": False
+    }
+    for box, class_id in zip(boxes.xyxy, boxes.cls):
+        class_id = int(class_id)
+        class_name = names[class_id]
+        x1, y1, x2, y2 = map(int, box)
+
+        if class_name == "Char":
+            character_last_seen = time.time()
+
+            char_details["x1"] = x1
+            char_details["y1"] = y1
+            char_details["x2"] = x2
+            char_details["y2"] = y2
+            char_details["character_here"] = True
+
+            char_x_avg = (x1 + x2) // 2
+            lower_x = monitor["width"] // 2 - middle_lane_room
+            upper_x = monitor["width"] // 2 + middle_lane_room
+            
+            if char_x_avg < lower_x:
+                lane = 0
+            elif char_x_avg > upper_x:
+                lane = 2
+            else:
+                lane = 1
+
+            current_lane = lane
 
     for box, class_id in zip(boxes.xyxy, boxes.cls):
         class_id = int(class_id)
         class_name = names[class_id]
-
         x1, y1, x2, y2 = map(int, box)
 
-        total_obsticales = []
+        if "Train" in class_name or "Obs" in class_name:
+            x1_avg = (x1 + x2) // 2
+            y1_avg = (y1 + y2) // 2
 
-        if y2 > 400 and "Train" in class_name:
-            lane = math.floor(((x1+x2)/2)/310)
-            print(f"Train, Lower Y (y2): {y2}")
-            print(f"LANE = {lane}")
-            total_obsticales.append({
-                "obs": "Train",
-                "lane": lane
-            })
+            char_x_avg = (char_details["x1"] + char_details["x2"]) // 2
+            obs_x_avg = (x1 + x2) // 2
+            
+            y_distance = y1_avg - char_details["y1"]
+            x_distance = obs_x_avg - char_x_avg
+            distance = math.sqrt(x_distance**2 + y_distance**2)
+            
+            measured_angle = math.degrees(math.atan2(y_distance, x_distance))
+            if -80 > measured_angle > -100 and distance < 170:
+                print(f"Class: {class_name}, Angle: {measured_angle}, Distance: {distance}")
 
-
-        if y2 > 400 and "Obs" in class_name:
-            lane = math.floor(((x1+x2)/2)/310)
-            obsi = class_name.replace('Obs_','')
-            print(f"{class_name.replace('Obs_','')}, Lower Y (y2): {y2}")
-            print(f"LANE = {lane}")
-            total_obsticales.append({
-                "obs": obsi,
-                "lane": lane
-            })
-
-        class_name = class_name.replace("Obs_", "")
-        class_name = "Train" if "Train" in class_name else class_name
-        (text_w, text_h), baseline = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
-        cv2.rectangle(frame, (x1, y1 - text_h - 6), (x1 + text_w + 4, y1), (0, 0, 0), -1)
-        cv2.putText(frame, class_name, (x1 + 2, y1 - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-        #Desicion Making
-        if len(total_obsticales) > 0:
-            lane_info = ["","",""]
-            for obstical in total_obsticales:
-                if obstical["obs"] == "Train":
-                    print(f"Train detected in lane {obstical['lane']}")
-                    lane_info[obstical["lane"]] = "T"
-                else:
-                    print(f"Obstacle {obstical['obs']} detected in lane {obstical['lane']}")
-                    print(obstical["lane"])
-                    lane_info[obstical["lane"]] = obstical["obs"][0]
-
-            if lane_info[current_lane] == "T":
-                print("Train detected in current lane")
-                if current_lane == 0:
-                    print("Switching to lane 1")
-                    current_lane = 1
-                    pyautogui.press('right')
-                elif current_lane == 1:
-                    if lane_info[0] == "" and lane_info[2] == "":
-                        choice = random.choice([0, 2])
-                        if choice == 0:
-                            pyautogui.press('left')
-
-                        else:
-                            pyautogui.press('right')
-                        current_lane = choice
-
-                    elif lane_info[2] == "T":
-                        print("Switching to lane 0")
-                        current_lane = 0
-                        pyautogui.press('left')
-                        
+                if "Train" in class_name:
+                    if current_lane == 0:
+                        pyautogui.press("right")
+                    elif current_lane == 2:
+                        pyautogui.press("left")
                     else:
-                        print("Switching to lane 2")
-                        current_lane = 2
-                        pyautogui.press('right')
+                        pyautogui.press("left")
+                
                 else:
-                    print("Switching to lane 0")
-                    pyautogui.press('left')
-                    current_lane = 1
-            
-            elif lane_info[current_lane] == "M" or lane_info[current_lane] == "L":
-                print("Moving obstacle detected in current lane")
-                pyautogui.press('up')
-            
-            elif lane_info[current_lane] == "H":
-                print("Moving obstacle detected in current lane")
-                pyautogui.press('down')
+                    if class_name == "Obs_High":
+                        pyautogui.press("down")
+                    elif class_name == "Obs_Low":
+                        pyautogui.press("up")
+                    else:
+                        pyautogui.press("up")
 
+            # print(f"Angle: {measured_angle}, Distance: {math.sqrt(x_distance**2 + y_distance**2)}")
 
     cv2.imshow("YOLOv8 Video Detection", frame)
 
